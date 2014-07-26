e141cc4c9196ffe382fba91a95417d83aeeb1e89
Yuri D'Elia <yuri.delia@eurac.edu>
2014-07-26 22:57:20 +0200

    Add proxying tests between PHP<>Perl.

---
+++ test/test_PHP_Perl.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+import bond
+
+def test_call_PHP_Perl():
+    php = bond.PHP.PHP(timeout=1)
+    perl = bond.Perl.Perl(timeout=1)
+    assert(php and perl)
+
+    php.eval(r'function func_php($arg) { return $arg + 1; }')
+    php.proxy('func_php', perl)
+
+    func_perl = perl.callable('func_php')
+    ret = func_perl(0)
+    assert(ret == 1)
+
+
+def test_call_Perl_PHP():
+    php = bond.PHP.PHP(timeout=1)
+    perl = bond.Perl.Perl(timeout=1)
+    assert(php and perl)
+
+    perl.eval(r'sub func_perl { shift() + 1; }')
+    perl.proxy('func_perl', php)
+
+    func_php = php.callable('func_perl')
+    ret = func_php(0)
+    assert(ret == 1)
