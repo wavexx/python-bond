65b8ced72186cf3897d2ae666f4b592c08f6388a
Yuri D'Elia <yuri.delia@eurac.edu>
2014-07-26 15:44:25 +0200

    Fix setup.

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
-from setuptools import setup
-setup(name='bond',
+from setuptools import setup, find_packages
+setup(name='python-bond',
+      description='Ambivalent bonds between Python and other languages',
       version='0.1',
-      packages=['bond'],
-      setup_requires=['nose'],
-      test_suite='nose.collector',
-      install_requires=['pexpect'])
+      url="https://github.com/wavexx/python-bond",
+      author="Yuri D'Elia",
+      author_email="wavexx@thregr.org",
+      license="GPL2",
+
+      packages=find_packages(),
+      include_package_data=True,
+      exclude_package_data = {'': ['.gitignore']},
+
+      install_requires=['pexpect'],
+
+      setup_requires=['nose', 'setuptools_git'],
+      test_suite='nose.collector')
