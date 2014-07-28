9e6d2dd80bf9187cda9c84eaa8b23bb9d5284559
Yuri D'Elia <yuri.delia@eurac.edu>
2014-07-28 15:29:01 +0200

    Refine setup for release.

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-setup(name='python-bond',
+setup(name='python-bond', version='0.1',
       description='transparent remote/recursive evaluation between Python and other languages',
-      version='0.1',
-      url="https://github.com/wavexx/python-bond",
+
       author="Yuri D'Elia",
       author_email="wavexx@thregr.org",
       license="GPL2",
+      long_description=open('README.rst').read(),
+      url="https://github.com/wavexx/python-bond",
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Console',
+                   'Environment :: No Input/Output (Daemon)',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Programming Language :: PHP',
+                   'Programming Language :: Perl',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Interpreters',
+                   'Topic :: Software Development'],
 
       packages=find_packages(),
       include_package_data=True,
       exclude_package_data = {'': ['.gitignore']},
 
-      install_requires=['pexpect'],
-
+      install_requires=['pexpect', 'pkg_resources'],
       setup_requires=['nose', 'setuptools_git'],
       test_suite='nose.collector')
