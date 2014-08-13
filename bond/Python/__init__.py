a316b0be536cd89e0ba316da229f9b46d7c7c767
Yuri D'Elia <yuri.delia@eurac.edu>
2014-08-13 17:39:42 +0200

    Deprecate old constructors.

--- bond/Python/__init__.py
+++ bond/Python/__init__.py
@@ -1,7 +1,15 @@
 from bond import *
 import os
+import shlex
+import warnings
 
 def Python(cmd=None, args=None, xargs='', cwd=None, env=os.environ,
-           trans_except=True, protocol=None, compat=None, timeout=None, logfile=None):
+           trans_except=True, protocol=None, compat=None, timeout=60, logfile=None):
+    warnings.warn("Language constructors are deprecated."
+                  " Please use ``bond.bond()`` directly.",
+                  DeprecationWarning)
+
+    if args is not None: args = shlex.split(args)
+    if xargs is not None: xargs = shlex.split(xargs)
     return bond('Python', cmd, args, xargs, cwd=cwd, env=env,
                 trans_except=trans_except, timeout=timeout, logfile=logfile)
