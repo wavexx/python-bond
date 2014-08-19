66485035c85c6033396d1f2a0027838195ed8bd5
Yuri D'Elia <yuri.delia@eurac.edu>
2014-08-19 17:49:56 +0200

    Travis: split api_v0 tests to get meaningful error messages.

--- tests/test_api_v0.py
+++ tests/test_api_v0.py
@@ -2,21 +2,28 @@ from __future__ import print_function
 import bond
 from tests import *
 
-def test_api_v0():
-    # TODO: language constructors (as supported in v0) are deprecated in favor
-    #       of the make_bond function.
+# TODO: language constructors (as supported in v0) are deprecated in favor
+#       of the make_bond function.
+
+def test_PHP():
     import bond.PHP
     php = bond.PHP.PHP(timeout=TIMEOUT)
     assert(php.eval('1') == 1)
 
+
+def test_Perl():
     import bond.Perl
     perl = bond.Perl.Perl(timeout=TIMEOUT)
     assert(perl.eval('1') == 1)
 
+
+def test_Python():
     import bond.Python
     py = bond.Python.Python(timeout=TIMEOUT)
     assert(py.eval('1') == 1)
 
+
+def test_JavaScript():
     import bond.JavaScript
     js = bond.JavaScript.JavaScript(timeout=TIMEOUT)
     assert(js.eval('1') == 1)
